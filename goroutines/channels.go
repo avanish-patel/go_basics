8f3a9d6a835b1e4dc44eac4d756f75cb59f1af4c
Avanish patel <avanishpatel@Avanishs-MacBook-Pro-2.local>
2020-06-05 23:10:58 -0500

    Added goroutines programs

---
+++ goroutines/channels.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func sum(s []int, c chan int) {
+    
+    sum := 0
+    for _, v := range s {
+        sum += v
+    }
+    // send to channel c
+    c <- sum
+}
+
+func main() {
+
+    s := []int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
+
+    c := make(chan int)
+
+    go sum(s[:len(s)/2],c)
+    go sum(s[len(s)/2:],c)
+    
+    // receive from channel
+    x, y := <-c, <-c
+
+    fmt.Println(x, y , x+y)
+}
